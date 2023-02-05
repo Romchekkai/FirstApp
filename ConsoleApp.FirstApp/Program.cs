4896a011bd14b7dcd2fa7c2b842b9fd4cf842edd
Romario123 <kajdenkoroman35052@gmail.com>
2023-02-05 16:52:10 +1000

    типы данных

--- ConsoleApp.FirstApp/Program.cs
+++ ConsoleApp.FirstApp/Program.cs
@@ -4,11 +4,19 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Привет,\n мир!");
 
-        Console.Write('\x23');
+        const string MyName = "Roma";
+
+        Console.WriteLine($"My name is {MyName}");
+        short age = 24;
+        Console.WriteLine($"My age is {age}");
+        bool pet = true;
+        Console.WriteLine($" Do i have a pet {pet}");
+        double shoesize = 42.5;
+        Console.WriteLine($"My shoe size is {shoesize}");
 
 
+    
 
     }
 }
