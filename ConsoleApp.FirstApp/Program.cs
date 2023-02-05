e80c99d28715920961d24f17539506436d8ce552
Romario123 <kajdenkoroman35052@gmail.com>
2023-02-05 21:05:49 +1000

    final

--- ConsoleApp.FirstApp/Program.cs
+++ ConsoleApp.FirstApp/Program.cs
@@ -4,15 +4,20 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        byte age = checked ((byte) int.Parse(Console.ReadLine()));
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-        Console.WriteLine("What is your favorite day of week&");
-        DayOfWeek day= (DayOfWeek)int.Parse(Console.ReadLine());
-        Console.WriteLine($"Your favorite day is {day}" );
-    }
-   
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
 
+        Console.WriteLine("How old are you?");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+
+        Console.Write("What is your favorite day of week? ");
+
+        var day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favorite day is {0}", day);
+        Console.WriteLine("What is your birthday?");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthday is {0}", birthdate);
+
+    }
 }
