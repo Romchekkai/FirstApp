98d8d4c39ce324c8b04d16dc86b65f1f2c646812
Romario123 <kajdenkoroman35052@gmail.com>
2023-02-10 19:27:45 +1000

    favorite color

--- ConsoleApp.FirstApp/Program.cs
+++ ConsoleApp.FirstApp/Program.cs
@@ -4,20 +4,8 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-
-        Console.WriteLine("How old are you?");
-        var age = checked((byte)int.Parse(Console.ReadLine()));
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-
-        Console.Write("What is your favorite day of week? ");
-
-        var day = (DayOfWeek)int.Parse(Console.ReadLine());
-        Console.WriteLine("Your favorite day is {0}", day);
-        Console.WriteLine("What is your birthday?");
-        var birthdate = Console.ReadLine();
-        Console.WriteLine("Your birthday is {0}", birthdate);
-
+        var inv = true;
+        var result = !inv;
+        Console.WriteLine(result);
     }
 }
